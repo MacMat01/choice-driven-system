51486f7afa41d3ea9774e63685db0fa24779f513
Matteo Machella <matteo.machella01@gmail.com>
2026-04-08 12:25:03 +0000

    Add SuppressMessage attribute to DataSchemaSO for naming consistency

--- Assets/Scripts/SchemaImporter/Schema/DataSchemaSO.cs
+++ Assets/Scripts/SchemaImporter/Schema/DataSchemaSO.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 namespace SchemaImporter.Schema
 {
@@ -6,20 +7,18 @@ namespace SchemaImporter.Schema
     ///     ScriptableObject that defines the schema for data import.
     ///     Designers define expected columns and must also assign a CSV/JSON source file.
     /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     [CreateAssetMenu(fileName = "DataSchema", menuName = "SchemaImporter/Data Schema")]
-    // ReSharper disable once InconsistentNaming
     public class DataSchemaSO : ScriptableObject
     {
         [Header("Schema Definition")]
         [Tooltip("List of expected columns and data types. Column names must match CSV/JSON keys.")]
         [SerializeField]
-        // ReSharper disable once InconsistentNaming
         private List<ColumnDefinition> columns = new List<ColumnDefinition>();
 
         [Header("Source File")]
         [Tooltip("CSV or JSON TextAsset used as the import source for this schema.")]
         [SerializeField]
-        // ReSharper disable once InconsistentNaming
         private TextAsset sourceDataFile;
 
         public List<ColumnDefinition> Columns => columns;
